8d17113d4f117dc1d546b1b04f1e3012216e83d5
Mark Jan van Kampen <mjvk@allseas.com>
2019-08-11 16:13:37 +0200

    Adds bincrafters

--- .ci/install.sh
+++ .ci/install.sh
@@ -5,5 +5,6 @@ set -x
 
 pip install conan --upgrade
 pip install conan_package_tools bincrafters_package_tools
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 
 conan user
