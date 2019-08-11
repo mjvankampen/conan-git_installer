5973e92416efa184383bcf4016d7d1b665e6dce1
Mark Jan van Kampen <mjvk@allseas.com>
2019-08-11 20:47:04 +0200

    And again

--- build.py
+++ build.py
@@ -5,6 +5,6 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     command = "conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan"
-    builder = ConanMultiPackager()
-    builder.add_common_builds(pure_c=True, docker_entry_script=command)
+    builder = ConanMultiPackager(docker_entry_script=command)
+    builder.add_common_builds(pure_c=True)
     builder.run()
